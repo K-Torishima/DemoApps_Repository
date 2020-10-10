95dabaefe05b218975d5c5e56a1b2c63c3b478ee
koji-torishima <torikou3685@gmail.com>
2020-10-10 20:42:31 +0900

    Memo

--- Swift_learning/delegate.playground/Contents.swift
+++ Swift_learning/delegate.playground/Contents.swift
@@ -5,5 +5,4 @@ import Foundation
 // 処理を委譲できる delegation
 // 委譲 (英: delegation) とはオブジェクト指向プログラミングにおいて、あるオブジェクトの操作を一部他のオブジェクトに代替させる手法のこと。
 // デリゲートとは委譲。他の何者かに、委（ゆだ）ね、譲（ゆず）ること
-
-
+// よってdelegateはprotocolではない。protpcolはinterface,delagateはあくまでデザインパターン
