71c1a75501e95416cb636a77cf233414bb8160b9
koji-torishima <torikou3685@gmail.com>
2020-10-10 20:40:20 +0900

    メモの追加

--- Swift_learning/delegate.playground/Contents.swift
+++ Swift_learning/delegate.playground/Contents.swift
@@ -1,3 +1,9 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// delegateとは
+// デザインパターンの一つ
+// 処理を委譲できる delegation
+// 委譲 (英: delegation) とはオブジェクト指向プログラミングにおいて、あるオブジェクトの操作を一部他のオブジェクトに代替させる手法のこと。
+// デリゲートとは委譲。他の何者かに、委（ゆだ）ね、譲（ゆず）ること
+
+
