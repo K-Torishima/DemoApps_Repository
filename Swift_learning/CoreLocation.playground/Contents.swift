3674d559f708b6601e2c73094b4087295592a793
koji-torishima <torikou3685@gmail.com>
2020-10-15 16:58:19 +0900

    class GeoCoordinateDelegate: NSObject, CLLocationManagerDelegate {         func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {         let location = locations[0]         print("Most Recent Location: " + location.description)         print(location.coordinate.latitude)         print(location.coordinate.longitude)     }         func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {         print("Error while updating " + error.localizedDescription)     }     } let geoCoordinateDelegate = GeoCoordinateDelegate() locationManager.delegate = geoCoordinateDelegate locationManager.desiredAccuracy = kCLLocationAccuracyBest locationManager.startUpdatingLocation()

--- Swift_learning/CoreLocation.playground/Contents.swift
+++ Swift_learning/CoreLocation.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
+import CoreLocation
+import PlaygroundSupport
 
-var str = "Hello, playground"
+let locationManager = CLLocationManager()
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class GeoCoordinateDelegate: NSObject, CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        print("Most Recent Location: " + location.description)
+        print(location.coordinate.latitude)
+        print(location.coordinate.longitude)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while updating " + error.localizedDescription)
+    }
+
+}
+
+let geoCoordinateDelegate = GeoCoordinateDelegate()
+locationManager.delegate = geoCoordinateDelegate
+locationManager.desiredAccuracy = kCLLocationAccuracyBest
+locationManager.startUpdatingLocation()
