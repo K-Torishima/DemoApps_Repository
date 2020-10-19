8e943e68e14b86f0bc95254b388986eab1a40ceb
koji-torishima <torikou3685@gmail.com>
2020-10-19 14:53:53 +0900

    追加

--- Swift_learning/Extension.playground/Contents.swift
+++ Swift_learning/Extension.playground/Contents.swift
@@ -30,3 +30,32 @@ extension Student {
     }
 }
 print(student.nameCount)
+
+struct Size {
+    var width = 0.0
+    var height = 0.0
+}
+struct Point {
+    var x = 0.0
+    var y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let menberwireRect = Rect(origin: Point(x: 2.0, y: 2.0),
+                          size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0),
+                      size: Size(width: 3.0, height: 3.0))
