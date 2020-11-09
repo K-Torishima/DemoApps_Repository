d50f40868fe5fa92101a07e6cb06ab3e350d6d8e
koji-torishima <torikou3685@gmail.com>
2020-11-09 20:35:57 +0900

    コミット

--- Swift_learning/URLSession.playground/Contents.swift
+++ Swift_learning/URLSession.playground/Contents.swift
@@ -26,19 +26,43 @@ public struct NasaImageApiItemData : Codable {
 
 
 class Client {
-    func someTask() {
-        let url = URL(string: "https://images-api.nasa.gov/search?media_type=image")!
+    let url = URL(string: "https://qiita.com/api/v2/items/a53b0b3f7e1bc7c06106/likes")!
+
+    // GET
+    internal func getTask() {
         let task = URLSession.shared.dataTask(with: url) { data, urlResponse, error in
             guard let data = data, let urlResponse = urlResponse as? HTTPURLResponse else {
                 return
             }
-            self.dacoder(data: data)
-            print("statusCode: \(urlResponse.statusCode)")
+            self.printJson(data)
+//            self.dacoder(data: data)
+//            print("statusCode: \(urlResponse.statusCode)")
+//            print("url: \(urlResponse.url)")
+//            print("mimeType: \(urlResponse.mimeType)")
+//            print("expectedContentLength: \(urlResponse.expectedContentLength)")
+//            print("textEncodingName: \(urlResponse.textEncodingName)")
+//            print("suggestedFilename: \(urlResponse.suggestedFilename)")
+//            print("allHeaderFields: \(urlResponse.allHeaderFields["Content-Type"] ?? "")")
         }
         task.resume()
     }
 
-    func printJson(_ data: Data) {
+    // POST
+    internal func postTask() {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = "".data(using: .utf8)
+        let session = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, let response = response as? HTTPURLResponse else { return }
+                print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+                print("statusCode: \(response.statusCode)")
+                print(String(data: data, encoding: .utf8) ?? "")
+
+        }
+        session.resume()
+    }
+
+    private func printJson(_ data: Data) {
         do {
             let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
             print(json)
@@ -47,7 +71,7 @@ class Client {
         }
     }
 
-    func dacoder(data: Data) {
+    private func dacoder(data: Data) {
         do {
             let decoder = JSONDecoder()
             let res = try decoder.decode(NasaImageApiSuccessResponse.self, from: data)
@@ -59,4 +83,5 @@ class Client {
 }
 
 let client = Client()
-client.someTask()
+client.getTask()
+client.postTask()
