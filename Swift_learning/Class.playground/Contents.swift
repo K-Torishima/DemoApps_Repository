e2fb4a7b77ffc0b5e39cca4080558e31bf2cddc1
koji-torishima <torikou3685@gmail.com>
2020-11-24 12:04:50 +0900

    class

--- Swift_learning/Class.playground/Contents.swift
+++ Swift_learning/Class.playground/Contents.swift
@@ -5,3 +5,20 @@ import Foundation
 // 構造体と列挙体と違うは　プロパティやメソッドの文法、初期化のフローなどが違う
 //　上記は継承を考慮するため
 //　CocoaフレームワークのほとんどはClassで定義されている
+
+class SomeClass {
+    let id: Int
+    let name: String
+
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+
+    func printName() {
+        print(name)
+    }
+}
+
+let instane = SomeClass(id: 1, name: "name")
+instane.printName()
