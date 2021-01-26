79ea4dab77e60aff925d08421ee9b86a2b8dcca5
koji-torishima <torikou3685@gmail.com>
2021-01-27 01:35:00 +0900

    init

---
+++ Dependency_Injection/DI_Sample/DI_Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI_Sample
+//
+//  Created by 鳥嶋 晃次 on 2021/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
