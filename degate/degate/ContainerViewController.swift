f2572a33dc4e586964c6302b2e8cad0334939563
koji-torishima <torikou3685@gmail.com>
2021-02-01 17:16:03 +0900

    コミット

---
+++ degate/degate/ContainerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContainerViewController.swift
+//  degate
+//
+//  Created by 鳥嶋 晃次 on 2021/02/01.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
