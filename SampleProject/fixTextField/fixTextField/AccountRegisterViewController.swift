4372f0657f8605ad6d5e00404b7b0dda8d971f90
koji-torishima <torikou3685@gmail.com>
2021-02-12 20:15:47 +0900

    バグ調査したのが終わった

---
+++ SampleProject/fixTextField/fixTextField/AccountRegisterViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  fixTextField
+//
+//  Created by 鳥嶋 晃次 on 2021/02/12.
+//
+
+import UIKit
+
+class AccountRegisterViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var f1: UITextField!
+    @IBOutlet weak var f2: UITextField!
+    @IBOutlet weak var f3: UITextField!
+    @IBOutlet weak var f4: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        f1.delegate = self
+        f2.delegate = self
+        f3.delegate = self
+        f4.delegate = self
+    }
+    
+    func initTextField() {
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == f1) {
+            f2?.becomeFirstResponder()
+        } else if (textField == f2) {
+            f3?.becomeFirstResponder()
+        } else if (textField == f3) {
+            f4?.becomeFirstResponder()
+        } else if (textField == f4) {
+            f1?.becomeFirstResponder()
+        }
+        return true
+    }
+
+
+}
+
