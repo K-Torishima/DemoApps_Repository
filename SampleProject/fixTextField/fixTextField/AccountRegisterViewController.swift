347e17423af9c7bf29c535b8247d98f22ff3f067
koji-torishima <torikou3685@gmail.com>
2021-02-14 13:39:10 +0900

    rename

--- SampleProject/fixTextField/fixTextField/AccountRegisterViewController.swift
+++ SampleProject/fixTextField/fixTextField/AccountRegisterViewController.swift
@@ -10,31 +10,31 @@ import UIKit
 // クラスの名前をViewContoroller　→　AccountRegisterViewControllerに変える発生する
 
 class AccountRegisterViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var f1: UITextField!
-    @IBOutlet weak var f2: UITextField!
-    @IBOutlet weak var f3: UITextField!
-    @IBOutlet weak var f4: UITextField!
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        f1.delegate = self
-        f2.delegate = self
-        f3.delegate = self
-        f4.delegate = self
+        textField1.delegate = self
+        textField2.delegate = self
+        textField3.delegate = self
+        textField4.delegate = self
     }
     
     func initTextField() {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if (textField == f1) {
-            f2?.becomeFirstResponder()
-        } else if (textField == f2) {
-            f3?.becomeFirstResponder()
-        } else if (textField == f3) {
-            f4?.becomeFirstResponder()
-        } else if (textField == f4) {
-            f1?.becomeFirstResponder()
+        if (textField == textField1) {
+            textField2?.becomeFirstResponder()
+        } else if (textField == textField2) {
+            textField3?.becomeFirstResponder()
+        } else if (textField == textField3) {
+            textField4?.becomeFirstResponder()
+        } else if (textField == textField4) {
+            textField1?.becomeFirstResponder()
         }
         return true
     }
