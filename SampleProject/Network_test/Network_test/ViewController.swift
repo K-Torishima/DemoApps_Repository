c497de793ad2713348d5239cc08e18fc727cb30a
koji-torishima <torikou3685@gmail.com>
2021-02-24 17:03:44 +0900

    コミット

--- SampleProject/Network_test/Network_test/ViewController.swift
+++ SampleProject/Network_test/Network_test/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import Network
 
 class ViewController: UIViewController {
-
+    
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "")
+    @IBOutlet weak var labl: UILabel!
+    @IBOutlet weak var sampleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let wifiMonitor = NWPathMonitor(requiredInterfaceType: .wifi)
+        print(wifiMonitor)
+        monitoring()
     }
-
-
+    
+    // UIの更新をしたい場合はメインスレッドで行う
+    // ネットワークが通じていたら　sampleViewは表示される
+    // ネットワークが切れていたら　sampleViewは表示されない
+    private func monitoring() {
+        monitor.pathUpdateHandler = { path in
+            DispatchQueue.main.async {
+                if path.status == .satisfied {
+                    self.labl.text = "Connected"
+                    self.sampleView.isHidden = false
+                    print("Connected")
+                } else {
+                    self.labl.text = "Not Connected"
+                    self.sampleView.isHidden = true
+                    print("Not Connected")
+                }
+            }
+        }
+        monitor.start(queue: queue)
+    }
 }
 
