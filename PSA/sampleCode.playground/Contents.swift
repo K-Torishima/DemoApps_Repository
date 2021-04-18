adfdd66b708544c9b25141786fa4645dfdb550b7
koji-torishima <torikou3685@gmail.com>
2021-04-19 01:16:28 +0900

    コミット

--- PSA/sampleCode.playground/Contents.swift
+++ PSA/sampleCode.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+struct Cat {
+    
+    private (set) var name: String?
+    
+    func meow() {
+        print("吾輩は猫である")
+        guard name != nil else { return notName() }
+        getName(name: name!)
+    }
+    
+    func notName() {
+        print("名前はまだ無い。")
+    }
+    
+    func getName(name: String) {
+        print("名前は " + name + "だ")
+    }
+}
+
+let cat = Cat()
+cat.meow()
+
+let dora = Cat(name: "ドラ")
+dora.meow()
+
+let tama = Cat(name: "たま")
+tama.meow()
+
+
+
