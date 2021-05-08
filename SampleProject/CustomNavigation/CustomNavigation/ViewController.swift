63e54a7649755ec50d044c3c2d5894b50def1b39
koji-torishima <torikou3685@gmail.com>
2021-05-09 08:04:20 +0900

    コミット

--- SampleProject/CustomNavigation/CustomNavigation/ViewController.swift
+++ SampleProject/CustomNavigation/CustomNavigation/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "main"
+        setNavigationBar()
     }
+    
+    private func setNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        
+        
+        let item1 = UIBarButtonItem(image: .add, style: .plain, target: self, action: #selector(taped))
+        let item2 = UIBarButtonItem(image: .checkmark, style: .plain, target: self, action: #selector(taped))
+//        let item3 = UIBarButtonItem(image: .actions, style: .plain, target: self, action: #selector(taped))
+//        let item4 = UIBarButtonItem(image: .remove, style: .plain, target: self, action: #selector(taped))
+//
+         // stackViewが存在するため勝手にオートレイアウトが効いてしまう
+         // NavigationItemを視覚的に詰めたい場合Imageにインセットを入れてあげれば良いがこれはあまり良くない
+         // 本来であればデフォルトの位置で実装するべきなんだと思う
+        item1.imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        item2.imageInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
+    
+        
+       
+    
 
-
+        navigationItem.setRightBarButtonItems([item1, item2], animated: true)
+    }
+    
+    @objc private func taped() {
+        print("taped")
+    }
 }
 
