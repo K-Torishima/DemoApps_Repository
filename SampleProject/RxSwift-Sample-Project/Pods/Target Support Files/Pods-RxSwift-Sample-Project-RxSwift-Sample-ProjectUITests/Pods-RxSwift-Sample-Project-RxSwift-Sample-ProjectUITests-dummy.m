a03deb67c434ebae8faaf421d0c1e76f719c4cd3
koji-torishima <torikou3685@gmail.com>
2021-09-02 13:02:04 +0900

    .gitignore is now working

--- SampleProject/RxSwift-Sample-Project/Pods/Target Support Files/Pods-RxSwift-Sample-Project-RxSwift-Sample-ProjectUITests/Pods-RxSwift-Sample-Project-RxSwift-Sample-ProjectUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwift_Sample_Project_RxSwift_Sample_ProjectUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwift_Sample_Project_RxSwift_Sample_ProjectUITests
-@end
