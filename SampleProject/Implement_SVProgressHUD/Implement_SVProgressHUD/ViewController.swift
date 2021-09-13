74dfea7ad4aedd80bf499dd208ec40433398b0cb
koji-torishima <torikou3685@gmail.com>
2021-09-14 04:34:38 +0900

    修正

--- SampleProject/Implement_SVProgressHUD/Implement_SVProgressHUD/ViewController.swift
+++ SampleProject/Implement_SVProgressHUD/Implement_SVProgressHUD/ViewController.swift
@@ -12,16 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // サイクルの色
+        set()
         SVProgressHUD.show()
-        
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             SVProgressHUD.dismiss()
         }
     }
     
-    
-
-
+    func set() {
+        SVProgressHUD.setForegroundColor(.cyan)
+        SVProgressHUD.setBackgroundColor(.clear)
+    }
 }
 
